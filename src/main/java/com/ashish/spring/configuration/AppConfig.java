3fce7e8eb2db76eca8733d73c363cf0a45ca15d1
Ashish Singh <ashishoist04@gmail.com>
2018-02-05 22:00:26 +0530

    Configuration Class for all Auto Component Scanning

---
+++ src/main/java/com/ashish/spring/configuration/AppConfig.java
@@ -0,0 +1,10 @@
+package com.ashish.spring.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.ashish.spring")
+public class AppConfig {
+
+}
